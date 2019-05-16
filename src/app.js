90cb2893eab24d53473a71138c8fc31f8cb102b5
hybridbad <hybridbad90@gmail.com>
2019-05-16 16:11:56 +0100

    commit before branching

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+
 new Vue({
   el: '#app',
   data: {
@@ -9,5 +11,19 @@ var app2 = new Vue({
   el: '#weather',
   data: {
     temperature: '20' + ' degrees'
+  }
+})
+
+var weatherData = new Vue({
+  el: '#weatherData',
+  data: {
+
+      temperature: 20
+  },
+
+  methods: {
+    increment () {
+      this.temperature++
+    }
   }
 })

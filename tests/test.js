a798da1e03b9cac0010944c2126138337a726cf8
Cosmin <mirceacosmin@gmail.com>
2019-05-16 14:57:04 +0100

    Simple test: nightwatch

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 module.exports = {
   'Vue js test': function(browser) {
-    browser.url('localhost:8080/').pause(3000);
+    browser
+      .url('localhost:8080/')
+      .assert.containsText('#weather', '20 degrees');
   }
 };

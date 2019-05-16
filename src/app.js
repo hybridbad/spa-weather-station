dfaf741675f98e53ecf29ceee8322110b7e7b7f2
Cosmin <mirceacosmin@gmail.com>
2019-05-17 00:45:42 +0100

    vue_learning1

--- src/app.js
+++ src/app.js
@@ -1,26 +1,47 @@
 var header = new Vue({
   el: '#header',
   data: {
-    message: 'Hello Vue.js!'
+    componentName: 'WeatherData',
+    message: 'Is on',
+    accentColor: 'accent-color',
+    header: 'header'
   }
 });
 
 var menu = new Vue({
   el: '#menu',
   data: {
-    message: 'Menu'
+    componentName: 'Menu',
+    list: ['Option 1', 'Option 2']
+  },
+  methods: {
+    retrieveWeatherData: function() {
+      alert('Weather data retrieved');
+    }
   }
 });
 
 var weatherData = new Vue({
   el: '#weatherData',
   data: {
-    temperature: 20
+    results: [],
+    isOnline: true,
+    componentName: '',
+    query: '',
+    emailMessage: '',
+    searchIndexes: [],
+    selectedSearchIndexes: ['temperature', 'pressure']
   },
-
   methods: {
-    increment() {
-      this.temperature++;
+    executeSearch: function(event) {
+      alert(
+        'Query: ' + this.query + '"Button:"' + event.target.innerText + '"'
+      );
     }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3000/api/data')
+      .then(response => (this.results = response.data.data.reverse()));
   }
 });

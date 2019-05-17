0a715b0fa10d9748fc054c871a2cb3166b906aae
hybridbad <hybridbad90@gmail.com>
2019-05-17 11:25:17 +0100

    Separate vue components to individual files
    New folder created for components files
    requested in index.html for simplicity currently

--- src/app.js
+++ src/app.js
@@ -1,47 +1,29 @@
-var header = new Vue({
-  el: '#header',
-  data: {
-    componentName: 'WeatherData',
-    message: 'Is on',
-    accentColor: 'accent-color',
-    header: 'header'
-  }
-});
 
-var menu = new Vue({
-  el: '#menu',
-  data: {
-    componentName: 'Menu',
-    list: ['Option 1', 'Option 2']
-  },
-  methods: {
-    retrieveWeatherData: function() {
-      alert('Weather data retrieved');
-    }
-  }
-});
+new Vue({ el: '#header' })
 
-var weatherData = new Vue({
-  el: '#weatherData',
-  data: {
-    results: [],
-    isOnline: true,
-    componentName: '',
-    query: '',
-    emailMessage: '',
-    searchIndexes: [],
-    selectedSearchIndexes: ['temperature', 'pressure']
-  },
-  methods: {
-    executeSearch: function(event) {
-      alert(
-        'Query: ' + this.query + '"Button:"' + event.target.innerText + '"'
-      );
-    }
-  },
-  mounted() {
-    axios
-      .get('http://localhost:3000/api/data')
-      .then(response => (this.results = response.data.data.reverse()));
-  }
-});
+new Vue({ el: '#menu' })
+
+new Vue({ el: '#weatherData'})
+
+// var header = new Vue({
+//   el: '#header',
+//   data: {
+//     componentName: 'WeatherData',
+//     message: 'Is on',
+//     accentColor: 'accent-color',
+//     header: 'header'
+//   }
+// });
+
+// var menu = new Vue({
+//   el: '#menu',
+//   data: {
+//     componentName: 'Menu',
+//     list: ['Option 1', 'Option 2']
+//   },
+//   methods: {
+//     retrieveWeatherData: function() {
+//       alert('Weather data retrieved');
+//     }
+//   }
+// });
